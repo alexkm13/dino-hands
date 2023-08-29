114ec587a15379e913295b5db2cb8224f1e2a23e
alexkm13 <95406259+alexkm13@users.noreply.github.com>
2023-08-28 21:46:26 -0400

    Updated dino.py
    Implemented a new approach which is more consistent and easier to explain for users.

--- dino.py
+++ dino.py
@@ -1,35 +1,51 @@
 import cv2
-import cvzone
 from cvzone.HandTrackingModule import HandDetector
 import pyautogui as auto
+import time
 
+detector=HandDetector(detectionCon=0.8, maxHands=1)
 
-cap = cv2.VideoCapture(0)
-hd = HandDetector(maxHands=1)
+time.sleep(2.0)
 
-while 1:
-    rt,frame = cap.read()
-    frame = cv2.resize(frame,(1080,720))
+current_key_pressed = set()
 
-    hand,frame = hd.findHands(frame)
+video=cv2.VideoCapture(0)
 
-    if hand:
-        hands = hand[0]
-        lmlist = hands['lmList']
-        # print(lmlist)
+while True:
+    ret, frame = video.read()
+    hands, img = detector.findHands(frame)
+    cv2.rectangle(img, (0, 480), (300, 425),(50, 50, 255), -2)
+    cv2.rectangle(img, (640, 480), (400, 425),(50, 50, 255), -2)
+    
+    if hands:
+        lmList = hands[0]
+        fingerUp = detector.fingersUp(lmList)
 
-        length,info , frame = hd.findDistance(lmlist[4][0:2],lmlist[8][0:2],frame)
-        length = round(length)
-
-        if length < 25:
+        if fingerUp == [0,0,0,0,0]:
+            cv2.putText(frame, 'Finger Count: 0', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Jumping', (440,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
             auto.press('up')
 
-
-    cv2.imshow('frame',frame)
+        if fingerUp == [0,1,0,0,0]:
+            cv2.putText(frame, 'Finger Count: 1', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Not Jumping', (420,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            
+        if fingerUp == [0,1,1,0,0]:
+            cv2.putText(frame, 'Finger Count: 2', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Not Jumping', (420,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+        if fingerUp == [0,1,1,1,0]:
+            cv2.putText(frame, 'Finger Count: 3', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Not Jumping', (420,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+        if fingerUp == [0,1,1,1,1]:
+            cv2.putText(frame, 'Finger Count: 4', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Not Jumping', (420,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+        if fingerUp == [1,1,1,1,1]:
+            cv2.putText(frame, 'Finger Count: 5', (20,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+            cv2.putText(frame, 'Not Jumping', (420,460), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 1, cv2.LINE_AA)
+    cv2.imshow("Frame",frame)
     x = cv2.waitKey(1)
-    if x == ord('q'):
+    if k == ord('q'):
         break
 
-cap.release()
+video.release()
 cv2.destroyAllWindows()
-
